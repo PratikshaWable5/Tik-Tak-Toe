98af30ffc9caffe0de718d814eb7f8476dd96df8
Pratiksha wable <68956014+PratikshaWable5@users.noreply.github.com>
2021-07-16 22:30:18 +0530

    Add files via upload

---
+++ Icon.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { FaTimes,FaRegCircle, FaPen  } from "react-icons/fa";
+
+const Icon = ({choice})=>{
+    switch(choice){
+        case "Cross":
+            return <FaTimes className="icon"/>;
+        case "Circle": 
+            return <FaRegCircle className="icon" />;
+        default:
+            return "";
+    }
+}
+export default Icon
